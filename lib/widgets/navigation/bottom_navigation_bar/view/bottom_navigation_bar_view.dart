db545222c04e837fc972aef8beb9aad32abdaab6
svlobachev <72977939+svlobachev@users.noreply.github.com>
2022-05-29 18:25:34 +0300

    added GetWidget module

--- lib/widgets/navigation/bottom_navigation_bar/view/bottom_navigation_bar_view.dart
+++ lib/widgets/navigation/bottom_navigation_bar/view/bottom_navigation_bar_view.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:telechat_proto/widgets/chats_list/model/chats_model.dart';
-import 'package:telechat_proto/widgets/chats_list/view/chats_view.dart';
-import 'package:telechat_proto/widgets/navigation/bottom_navigation_bar/controller/MyFloatingActionButtonController.dart';
-import 'package:telechat_proto/widgets/topics_list/model/topics_model.dart';
-import 'package:telechat_proto/widgets/topics_list/view/topics_view.dart';
+import 'package:getwidget/getwidget.dart';
+
+import '../../../chats_list/model/chats_model.dart';
+import '../../../chats_list/view/chats_view.dart';
+import '../../../topics_list/model/topics_model.dart';
+import '../../../topics_list/view/topics_view.dart';
+import '../controller/MyFloatingActionButtonController.dart';
+import '../controller/MyTabController.dart';
 
 MyFloatingActionButtonController myFloatingActionButtonController =
     Get.put(MyFloatingActionButtonController());
 
 class MyBottomNavigationBar extends StatelessWidget {
   MyBottomNavigationBar({Key? key}) : super(key: key);
-
-  final RxInt _selectedIndex = 0.obs;
+  final MyTabController _tabx = Get.put(MyTabController());
 
   Rx<MaterialColor> _myCollor = Colors.red.obs;
   Image _myIcon = Image.asset(
@@ -20,31 +22,11 @@ class MyBottomNavigationBar extends StatelessWidget {
     height: 40,
   );
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-    ChatsView(
-      items: ChatsModel().items,
-    ),
-    Text(
-      'Index 2',
-      style: optionStyle,
-    ),
-    TopicsView(
-      items: TopicsModel().items,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    void _onItemTapped(int index) {
-      _selectedIndex.value = index;
-    }
-
-    return Obx(() => Scaffold(
-          floatingActionButton: FloatingActionButton(
+    return Scaffold(
+      floatingActionButton: Obx(() => FloatingActionButton(
             onPressed: () {
-              // myFloatingActionButtonController.setContext = context;
               myFloatingActionButtonController.setMyCollor = _myCollor;
               myFloatingActionButtonController.setMyIcon = _myIcon;
               _myCollor = myFloatingActionButtonController.getMyColor;
@@ -53,30 +35,37 @@ class MyBottomNavigationBar extends StatelessWidget {
             elevation: 5,
             backgroundColor: _myCollor.value,
             child: _myIcon,
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: GFTabBarView(controller: _tabx.controller, children: <Widget>[
+        Container(
+          child: ChatsView(
+            items: ChatsModel().items,
           ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-          body: Center(
-            child: _widgetOptions.elementAt(_selectedIndex.value),
+        ),
+        Container(
+          child: const Text(
+            'Index 2',
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.chat_bubble),
-                label: 'chats'.tr,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.radar),
-                label: 'radar'.tr,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.theater_comedy),
-                label: "myTopics".tr,
-              ),
-            ],
-            currentIndex: _selectedIndex.value,
-            selectedItemColor: Colors.amber[800],
-            onTap: _onItemTapped,
+        ),
+        Container(
+          child: TopicsView(
+            items: TopicsModel().items,
           ),
-        ));
+        ),
+      ]),
+      bottomNavigationBar: GFTabBar(
+        tabBarHeight: 70.0,
+        tabBarColor: Theme.of(context).colorScheme.primary,
+        indicatorColor: Colors.deepOrange,
+        labelColor: Theme.of(context).colorScheme.onPrimary,
+        unselectedLabelColor: Theme.of(context).colorScheme.secondaryContainer,
+
+        length: 3,
+        controller: _tabx.controller,
+        tabs: _tabx.myTabs,
+        // tabBarHeight: 70,
+      ),
+    );
   }
 }
